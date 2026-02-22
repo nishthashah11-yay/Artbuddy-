11ea0293e2b362dd4d7122f93fc8a6c16b0b9ef0
nishthashah11-yay <shahnishthap@gmail.com>
2026-02-22 05:16:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -64,14 +64,6 @@ if not st.session_state.splash_done:
 if st.session_state.page == "landing":
 
     st.markdown("""
-    # 🎨 Starry Night Image
-starry_night_url = "https://upload.wikimedia.org/wikipedia/commons/e/ee/The_Starry_Night.jpg"
-
-st.image(
-    starry_night_url,
-    caption="The Starry Night (1889) - Vincent van Gogh",
-    use_container_width=True
-)
     <div style="text-align:center;padding-top:120px;">
         <h1 style="font-size:52px;color:#4a148c;">AI-Powered Art Restoration Assistant</h1>
         <p style="font-size:20px;color:#6a1b9a;">
